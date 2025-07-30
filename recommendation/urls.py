17c637f798d0557ddde31a9fb20c7a9cf34ae3e4
Gauri Wankhade <66548889+gauriwankhade@users.noreply.github.com>
2025-07-30 14:23:08 +0530

    Update urls.py

--- recommendation/urls.py
+++ recommendation/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import RecommendView
+from .views import RecommendView, RecommendWithVectorDbView
 from .auth_views import RegisterView, LoginView
 
 urlpatterns = [
-  path('recommend/', RecommendView.as_view()),
+  path('recommend', RecommendView.as_view()),
+  path('recommend/rag/', RecommendWithVectorDbView.as_view()),
   path('register/', RegisterView.as_view()),
   path('login/', LoginView.as_view()),
 ]
+
+
